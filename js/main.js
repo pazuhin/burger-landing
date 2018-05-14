fb9811341fbbf0e7c0595b964126766c8fe10112
Andrey Smirnov <andrew_1024@mail.ru>
2018-05-14 20:40:01 +0300

    add mobil page and js menu

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var  btn = document.getElementById('menu__link');
+var  mobilPage = document.querySelector("#mobil-page");
+const  btnClose = document.querySelector('.close');
+let currentTop = 0;
+
+btn.addEventListener('click', function(e) {
+ e.preventDefault();
+ mobilPage.style.top = currentTop + 'px';
+});
+
+btnClose.addEventListener('click', function(e) {
+  e.preventDefault();
+  mobilPage.style.top = currentTop + 900 + 'px';
+});
