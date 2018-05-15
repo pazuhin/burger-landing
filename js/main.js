bab9a1e38f93786332b322e661ab3984253c2478
Andrey Smirnov <andrew_1024@mail.ru>
2018-05-15 21:43:01 +0300

    acco-team

--- js/main.js
+++ js/main.js
@@ -12,3 +12,27 @@ btnClose.addEventListener('click', function(e) {
   e.preventDefault();
   mobilPage.style.top = currentTop + 9999 + 'px';
 });
+
+
+//acco-team
+
+var teamAcco = document.querySelector('.team-acco');
+
+
+teamAcco.addEventListener('click', function(e) {
+  e.preventDefault();
+  var target = e.target;
+  var teamAccoItem = document.querySelector('.team-acco__item');
+
+  if(target.classList.contains('team-acco__trigger')) {
+
+    var content = target.nextElementSibling;
+    var contentHeight = parseInt(content.style.height);
+
+    if(contentHeight) {
+      content.style.height = 0;
+    } else {
+      content.style.height = content.scrollHeight + 'px';
+    }
+  }
+});
