6ca6b471b8fa194eb9aa9ed6ca6155b6f68c52fb
Andrey Smirnov <andrew_1024@mail.ru>
2018-05-17 20:32:56 +0300

    complite acco-team

--- js/main.js
+++ js/main.js
@@ -17,6 +17,9 @@ btnClose.addEventListener('click', function(e) {
 //acco-team
 
 var teamAcco = document.querySelector('.team-acco');
+var items = teamAcco.getElementsByClassName('team-acco__item');
+var contents = teamAcco.getElementsByClassName('person');
+var section = document.querySelector('.team__section');
 
 
 teamAcco.addEventListener('click', function(e) {
@@ -27,12 +30,35 @@ teamAcco.addEventListener('click', function(e) {
   if(target.classList.contains('team-acco__trigger')) {
 
     var content = target.nextElementSibling;
+    var item = target.parentNode;
     var contentHeight = parseInt(content.style.height);
 
-    if(contentHeight) {
-      content.style.height = 0;
-    } else {
+    if(!item.classList.contains('team-acco__item--active')) {
+
+      for (var i = 0; i < items.length; i++) {
+        items[i].classList.remove('team-acco__item--active');
+      }
+      item.classList.add('team-acco__item--active');
+
+      for (var i = 0; i < items.length; i++) {
+        contents[i].style.height = 0;
+      }
+
       content.style.height = content.scrollHeight + 'px';
+    } else {
+      item.classList.remove('team-acco__item--active');
+      content.style.height = 0;
     }
   }
+});
+
+section.addEventListener('click', function(e) {
+  var targetClose = e.target;
+
+  if(!targetClose.classList.contains('team-acco__trigger')) {
+      for (var i = 0; i < items.length; i++) {
+        items[i].classList.remove('team-acco__item--active');
+        contents[i].style.height = 0;
+      }
+    }
 });
