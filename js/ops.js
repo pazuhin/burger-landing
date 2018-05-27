0cc2aa0202a25294241204432f7e32b08dc57b9f
Andrey Smirnov <andrew_1024@mail.ru>
2018-05-27 18:28:51 +0300

    disable OPS for mobil

--- js/ops.js
+++ js/ops.js
@@ -3,84 +3,89 @@
 const sections = $(".section");
 const display = $(".maincontent");
 let inScroll = false;
-// var md = new MobileDetect(window.navigator.userAgent);
-// const isMobile = md.mobile();
+var md = new MobileDetect(window.navigator.userAgent);
+const isMobile = md.mobile();
 
-const setActiveMenuItem = itemEq => {
-  $('.sidebar__item').eq(itemEq).addClass('active').siblings().removeClass('active');
-}
+if (!isMobile) {
 
-const performTransition = sectionEq => {
-  const position = `${-sectionEq * 100}%`;
+  const setActiveMenuItem = itemEq => {
+    $('.sidebar__item').eq(itemEq).addClass('active').siblings().removeClass('active');
+  }
 
-  if (inScroll) return;
+  const performTransition = sectionEq => {
+    const position = `${-sectionEq * 100}%`;
 
-  inScroll = true;
-  sections.eq(sectionEq).addClass('active').siblings().removeClass('active');
+    if (inScroll) return;
 
-  display.css({
-    transform: `translate(0, ${position})`,
-    "-webkit-transform": `translate(0, ${position})`
+    inScroll = true;
+    sections.eq(sectionEq).addClass('active').siblings().removeClass('active');
+
+    display.css({
+      transform: `translate(0, ${position})`,
+      "-webkit-transform": `translate(0, ${position})`
+    });
+
+    const transitionDuration = parseInt(display.css('transition-duration')) * 1000; // получаем время в мс
+
+     setTimeout(() => {
+       inScroll = false;
+       setActiveMenuItem(sectionEq)
+     }, transitionDuration + 300);
+
+  };
+
+  const scrollToSection = direction => {
+    const activeSection = sections.filter('.active');
+    const nextSection = activeSection.next();
+    const prevSection = activeSection.prev();
+
+    if (direction === 'up' && prevSection.length) {
+      performTransition(prevSection.index());
+    }
+
+    else if (direction === 'down' && nextSection.length) {
+      performTransition(nextSection.index());
+    }
+
+  }
+
+  $(document).on({
+    wheel: e => {
+    console.log(e);
+      const deltaY = e.originalEvent.deltaY;
+      if (deltaY > 0) {
+        scrollToSection('down');
+        console.log('down');
+      } else {
+          scrollToSection('up');
+          console.log('up');
+      }
+    },
+    // touchmove: e => e.preventDefault()
   });
 
-  const transitionDuration = parseInt(display.css('transition-duration')) * 1000; // получаем время в мс
+  document.addEventListener("keydown", function(e){
+    console.log(e.keyCode);
+    switch (e.keyCode) {
+        case 40:
+          scrollToSection("down");
+          break;
 
-   setTimeout(() => {
-     inScroll = false;
-     setActiveMenuItem(sectionEq)
-   }, transitionDuration + 300);
+        case 38:
+          scrollToSection("up");
+          break;
+      }
+  });
 
-};
+  $('[data-scroll-to]').on('click', e => {
+    e.preventDefault();
+    const targer = $(e.currentTarget).data('scroll-to');
+    performTransition(targer);
 
-const scrollToSection = direction => {
-  const activeSection = sections.filter('.active');
-  const nextSection = activeSection.next();
-  const prevSection = activeSection.prev();
-
-  if (direction === 'up' && prevSection.length) {
-    performTransition(prevSection.index());
-  }
-
-  else if (direction === 'down' && nextSection.length) {
-    performTransition(nextSection.index());
-  }
+  })
 
 }
 
-$(document).on({
-  wheel: e => {
-  console.log(e);
-    const deltaY = e.originalEvent.deltaY;
-    if (deltaY > 0) {
-      scrollToSection('down');
-      console.log('down');
-    } else {
-        scrollToSection('up');
-        console.log('up');
-    }
-  },
-  // touchmove: e => e.preventDefault()
-});
-
-document.addEventListener("keydown", function(e){
-  console.log(e.keyCode);
-  switch (e.keyCode) {
-      case 40:
-        scrollToSection("down");
-        break;
-
-      case 38:
-        scrollToSection("up");
-        break;
-    }
-});
-
-$('[data-scroll-to]').on('click', e => {
-  e.preventDefault();
-  const targer = $(e.currentTarget).data('scroll-to');
-  performTransition(targer);
-
-})
 
 // const mobilMenuLink = $('.mobil-menu__link');
 // mobilMenuLink.addEventListener("click", function(e){
